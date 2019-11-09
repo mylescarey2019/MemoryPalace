42a8669b8deeaee95c62677ae3a5fe3e10371375
Myles Carey <seanmyles@comcast.net>
2019-11-09 14:53:47 -0800

    Updates

---
+++ precache-manifest.262e416ecd3e87aed7031ac98e638e4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68d74573950f0b2f2657c811ea1efd30",
+    "url": "/MemoryPalace/index.html"
+  },
+  {
+    "revision": "8d41aff9bcdb83a6d82f",
+    "url": "/MemoryPalace/static/css/main.ae2befcc.chunk.css"
+  },
+  {
+    "revision": "66c0549b65da3b383957",
+    "url": "/MemoryPalace/static/js/2.fda1d886.chunk.js"
+  },
+  {
+    "revision": "8d41aff9bcdb83a6d82f",
+    "url": "/MemoryPalace/static/js/main.b55fc351.chunk.js"
+  },
+  {
+    "revision": "68246ba0f08d7788b9f7",
+    "url": "/MemoryPalace/static/js/runtime-main.d3c180f2.js"
+  },
+  {
+    "revision": "0e33c6da6c510e26442d82def53abfd7",
+    "url": "/MemoryPalace/static/media/martin-jernberg-nE2gf1scItI-unsplash2.0e33c6da.jpg"
+  }
+]);
